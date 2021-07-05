3a09f7704bdc17a10809ab59b69a8fe327c7863c
LanceStasinski <lance.stasinski@gmail.com>
2021-07-05 10:56:40 -0400

    chore: Setup battleship pseudocode

---
+++ battleship-game/game.js
@@ -0,0 +1,26 @@
+//locations for 3 cells for a ship
+let location1
+let location2
+let location3
+
+let guess
+let hits = 0
+let isSunk = false
+
+while (isSunk == false) {
+  guess = prompt()
+  //compare input to valid input (integers)
+
+  // if (guess is invalid) {
+  //   alert('Please enter valid number')
+  // } else {
+  //   guess ++;
+  //   if (guess == locations) {
+  //     hits ++;
+  //     if (hits == 3) {
+  //       isSunk = true;
+  //       alert('You sank my battleship!')
+  //     }
+  //   }
+  // }
+}
