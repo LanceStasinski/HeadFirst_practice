be214554095a641dba6be2984b156e1c7422199e
LanceStasinski <lance.stasinski@gmail.com>
2021-07-05 11:27:03 -0400

    feat: Complete while loop

--- battleship-game/game.js
+++ battleship-game/game.js
@@ -1,26 +1,27 @@
 //locations for 3 cells for a ship
-let location1
-let location2
-let location3
+let location1 = 3
+let location2 = 4
+let location3 = 5
 
 let guess
 let hits = 0
+let guesses = 0
+
 let isSunk = false
 
 while (isSunk == false) {
-  guess = prompt()
-  //compare input to valid input (integers)
+  guess = prompt('Ready, aim, fire! (enter a number from 0-6:')
 
-  // if (guess is invalid) {
-  //   alert('Please enter valid number')
-  // } else {
-  //   guess ++;
-  //   if (guess == locations) {
-  //     hits ++;
-  //     if (hits == 3) {
-  //       isSunk = true;
-  //       alert('You sank my battleship!')
-  //     }
-  //   }
-  // }
+  if (guess < 0 || guess > 6) {
+    alert('Please enter valid number')
+  } else {
+    guess ++;
+    if (guess == location1 || guess == location2 || guess == location3) {
+      hits ++;
+    }
+    if (hits == 3) {
+      isSunk = true;
+      alert('You sank my battleship!')
+    }
+  }
 }
