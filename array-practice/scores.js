08a0e35376b6aa9f6c7076895afbfd5b575e9c40
LanceStasinski <lance.stasinski@gmail.com>
2021-07-06 14:18:06 -0400

    feat: Turn for loops into functions and add costs array

--- array-practice/scores.js
+++ array-practice/scores.js
@@ -1,25 +1,38 @@
-const scores = [1, 55, 33, 23, 56, 78, 56, 89, 99, 69, 55, 54, 52, 2, 45, 23, 12,
-                45, 22, 25, 69, 47, 85, 22, 99, 66, 77, 88, 66, 33, 22, 11, 44]
+const scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
+                34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
+                46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44]
 
-let output;
-let highScore = 0
-let highestScores = []
+const costs = [.25, .27, .25, .25, .25, .25,
+               .33, .31, .25, .29, .27, .22,
+               .31, .25, .25, .33, .21, .25,
+               .25, .25, .28, .25, .24, .22,
+               .20, .25, .30, .25, .24, .25,
+               .25, .25, .27, .25, .26, .29]
 
-for (let i = 0; i < scores.length; i++) {
-  console.log(`Solution ${[i]} score: ${score[i]}`)
-  if (scores[i] > highScore) {
-    highScore = scores[i]
+
+const printAndGetHighScore = (scores) => {
+  let highScore = 0;
+  for (let i = 0; i < scores.length; i++) {
+    console.log(`Solution ${[i]} score: ${scores[i]}`)
+    if (scores[i] > highScore) {
+      highScore = scores[i]
+    }
   }
+  return highScore;
 }
 
-for (let i = 0; i < scores.length; i++) {
-  if (scores[i] == highScore) {
-    highestScores.push(i)
+const getHighestScores = (scores, highScore) => {
+  let highestScores = []
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+      highestScores.push(i)
+    }
   }
+  return highestScores;
 }
 
-
-nTests = scores.length
-console.log(`Tests: ${ntests}`)
+const highScore = printAndGetHighScore(scores)
+const highestScores = getHighestScores(scores, highScore)
+console.log(`Tests: ${scores.length}`)
 console.log(`Highest score: ${highScore}`)
 console.log(`Solutions with the highest scores: ${highestScores}`)
