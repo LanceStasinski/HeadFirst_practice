551266ba485e713d47ed682d11238817d4eee38a
LanceStasinski <lance.stasinski@gmail.com>
2021-07-07 13:11:18 -0400

    feat: Add functions to taxi object

--- object-practice/prequal.js
+++ object-practice/prequal.js
@@ -5,22 +5,41 @@ const taxi = {
   color: 'yellow',
   passengers: 4,
   convertible: false,
-  mileage: 281341
-}
-
-const prequal = (car) => {
-  if (car.mileage > 10000) {
-    return false;
-  } else if (car.year > 1960) {
-    return false;
+  mileage: 281341,
+  started: false,
+  start: () => {
+    started = true;
+  },
+  stop: () => {
+    started = false;
+  },
+  drive: () => {
+    if (started) {
+      alert('Zoom zoom')
+    } else {
+      alert('You need to start the engine first')
+    }
   }
-  return true;
 }
 
-let worthALook = prequal(taxi)
+taxi.drive()
+taxi.start()
+taxi.drive()
+taxi.stop()
 
-if (worthALook) {
-  console.log(`You gotta check out this ${car.make} ${car.model}`)
-} else {
-  console.log(`You should really pass on the ${car.make} ${car.model}`)
-}
+// const prequal = (car) => {
+//   if (car.mileage > 10000) {
+//     return false;
+//   } else if (car.year > 1960) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// let worthALook = prequal(taxi)
+
+// if (worthALook) {
+//   console.log(`You gotta check out this ${car.make} ${car.model}`)
+// } else {
+//   console.log(`You should really pass on the ${car.make} ${car.model}`)
+// }
