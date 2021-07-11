448f27ca790c57fd10c9c7ab6f10de936be7e06d
LanceStasinski <lance.stasinski@gmail.com>
2021-07-11 10:19:56 -0400

    feat: Use an object within a closure

--- closures/counter.js
+++ closures/counter.js
@@ -1,13 +1,14 @@
 function makeCounter() {
   let count = 0;
 
-  function counter() {
-    count++;
-    return count;
+  return {
+    increment: function () {
+      count ++;
+      return count;
+    }
   }
-  return counter;
 }
 
 const doCount = makeCounter();
-console.log(doCount())
-console.log(doCount())
+console.log(doCount.increment())
+console.log(doCount.increment())
