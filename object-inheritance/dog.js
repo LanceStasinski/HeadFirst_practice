0ac920296b15743ade5a9b757b8f2db68d80c7ce
LanceStasinski <lance.stasinski@gmail.com>
2021-07-12 15:41:02 -0400

    feat: Practice object prototypal inheritance

---
+++ object-inheritance/dog.js
@@ -0,0 +1,49 @@
+function Dog(name, breed, weight) {
+  this.name = name;
+  this.breed = breed;
+  this. weight = weight;
+}
+
+Dog.prototype.species = 'canine';
+Dog.prototype.bark = function(){
+  console.log('bark')
+}
+Dog.prototype.wag = function() {
+  console.log('wag tail')
+}
+Dog.prototype.run = function() {
+  console.log('run')
+}
+
+
+function ShowDog(name, breed, weight, handler) {
+  this.name = name;
+  this.breed = breed;
+  this.weight = weight;
+  this.handler = handler;
+}
+
+
+ShowDog.prototype = new Dog();
+
+ShowDog.prototype.league = 'webville';
+ShowDog.prototype.stack = function () {
+  console.log('stack')
+}
+ShowDog.prototype.bait = function() {
+  console.log('bait')
+}
+ShowDog.prototype.gait = function() {
+  console.log('gait')
+}
+ShowDog.prototype.groom = function() {
+  console.log('groom')
+}
+
+const scottie = new ShowDog('Scottie', 'Scottish Terrier', 15, 'Cookie')
+
+//test
+scottie.stack()
+scottie.bark()
+console.log(scottie.league)
+console.log(scottie.species)
